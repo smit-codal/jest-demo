d56e0a41fb4807c2f9c03fecd57d7a445cb759e6
sraghani <sraghani@codal.com>
2023-06-06 14:49:52 +0530

    completed user details test cases

--- src/__tests__/pages/users/user-details.test.tsx
+++ src/__tests__/pages/users/user-details.test.tsx
@@ -1,29 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import UserDetails from "../../../pages/users/detail/user-details";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as JestRouter from "react-router";
+import { act } from "react-dom/test-utils";
 
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useParams: jest.fn(),
+}));
 
 jest.mock("../../../apiUtils", () => {
   const user = {
-    "id": 10,
-    "email": "smit@codal.com",
-    "first_name": "Smit",
-    "last_name": "Raghani",
-    "avatar": "https://reqres.in/img/faces/3-image.jpg"
-}
+    id: 10,
+    email: "smit@codal.com",
+    first_name: "Smit",
+    last_name: "Raghani",
+    avatar: "https://reqres.in/img/faces/3-image.jpg",
+  };
   return {
-    fetchUserDetail: () => Promise.resolve(user)
-  }
-})
+    fetchUserDetail: () => Promise.resolve(user),
+  };
+});
 
 describe("User Details:", () => {
   it("renders correctly", async () => {
-    render(
-      <Router>
-        <UserDetails />
-      </Router>
-    );
-
+    jest.spyOn(JestRouter, "useParams").mockReturnValue({ id: "1" });
+    const setUserDetail = jest.fn();
+    await act(async () => {
+      render(
+        <Router>
+          <UserDetails />
+        </Router>
+      );
+    });
+    setUserDetail.mockImplementation(() => {
+      expect(setUserDetail).toHaveBeenCalled();
+    });
     expect(screen.getByRole("heading")).toBeInTheDocument();
+    const emailField = screen.getByLabelText("Email:");
+    fireEvent.change(emailField, { target: { value: "smit@codal.com" } });
   });
 });
